3e1a14d03506e1c350b41b58a79a8a31d0dcb476
alek <wait82985@gmail.com>
2023-10-01 12:55:36 +0600

    problem solved

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,5 +1,33 @@
+import java.util.Arrays;
+
 public class Solution {
     public String reverseWords(String s) {
-        return null;
+        char[] chars = s.toCharArray();
+        int start = 0;
+        int end = 0;
+        for (int i = 0; i < chars.length; i++) {
+            if(chars[i] == ' ' || i == chars.length- 1){
+                if(i == chars.length- 1){
+                    reverseWord(chars, start, end);
+                    break;
+                }else {
+                    reverseWord(chars, start, end - 1);
+                }
+                start = i + 1;
+                end = i + 1;
+            }else {
+                end++;
+            }
+
+        }
+
+        return new String(chars);
+    }
+    private void reverseWord(char[] chars, int start, int end){
+        for (int i =0; i <= (end - start) / 2; i++) {
+            char temp = chars[start + i];
+            chars[start + i] = chars[end - i];
+            chars[end - i] = temp;
+        }
     }
 }
